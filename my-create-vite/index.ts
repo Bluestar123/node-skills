351d5122aedb9c6d42b5af9feb697f52e6fd2297
jihui.wang <jihui.wang@okg.com>
2024-10-31 22:51:27 +0800

    vite prompts template

--- my-create-vite/index.ts
+++ my-create-vite/index.ts
@@ -1,6 +1,8 @@
 import chalk from 'chalk'
 import minimist from 'minimist'
+import path from 'path'
 import prompts from 'prompts'
+import { fileURLToPath } from 'url'
 
 const argv = minimist<{ templete?: string; help?: boolean }>(
   process.argv.slice(2),
@@ -33,6 +35,62 @@ ${chalk.blueBright('qwik-ts        qwik')}`
 
 const defaultTargetDir = 'vite-project'
 
+type FrameworkVariant = {
+  name: string
+  display: string
+  color: Function
+  customCommand?: string
+}
+
+type Framework = {
+  name: string
+  display: string
+  color: Function
+  variants: FrameworkVariant[]
+}
+
+const FRAMEWORKS: Framework[] = [
+  {
+    name: 'vue',
+    display: 'Vue',
+    color: chalk.green,
+    variants: [
+      { name: 'vue-ts', display: 'TypeScript', color: chalk.blue },
+      { name: 'vue', display: 'JavaScript', color: chalk.yellow },
+    ],
+  },
+  {
+    name: 'react',
+    display: 'React',
+    color: chalk.cyan,
+    variants: [
+      {
+        name: 'react-ts',
+        display: 'TypeScript',
+        color: chalk.blue,
+      },
+      {
+        name: 'react-swc-ts',
+        display: 'TypeScript + SWC',
+        color: chalk.blue,
+      },
+      {
+        name: 'react',
+        display: 'JavaScript',
+        color: chalk.yellow,
+      },
+      {
+        name: 'react-swc',
+        display: 'JavaScript + SWC',
+        color: chalk.yellow,
+      },
+    ],
+  },
+]
+const TEMPLATES = FRAMEWORKS.map((f) => {
+  return f.variants.map((v) => v.name)
+}).reduce((acc, cur) => acc.concat(cur), [])
+
 function formatTargetDir(targetDirt: string | undefined) {
   // aaa/ => aaa
   return targetDirt?.trim().replace(/\/+$/g, '')
@@ -61,6 +119,33 @@ const init = async () => {
             targetDir = formatTargetDir(state.value) || defaultTargetDir
           },
         },
+        {
+          type:
+            argTemplate && TEMPLATES.includes(argTemplate) ? null : 'select',
+          name: 'framework',
+          message: chalk.reset('Select a framework:'),
+          initial: 0,
+          choices: FRAMEWORKS.map((f) => ({
+            title: f.color(f.display || f.name),
+            value: f,
+          })),
+        },
+        {
+          type: (framework: Framework) => {
+            // 如果是函数，会传入上一个问题的结果， 所以上面那个是 value: f
+            return framework && framework.variants ? 'select' : null
+          },
+          name: 'variant',
+          message: chalk.reset('Select a variant:'),
+          choices: (framework: Framework) => {
+            return framework.variants.map((v) => {
+              return {
+                title: framework.color(v.display || v.name),
+                value: v.name,
+              }
+            })
+          },
+        },
       ],
       {
         onCancel: () => {
@@ -74,6 +159,15 @@ const init = async () => {
   }
 
   console.log(result, targetDir)
+  const { framework, variant, projectName } = result
+  const root = path.join(process.cwd(), targetDir)
+  let template: string = variant || argTemplate
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url), // import.meta.url 是当前模块的路径，但是file:///, 需要fileURLToPath转换为文件路径
+    '../../..',
+    `template-${template}`
+  )
+  console.log(templateDir)
 }
 
 init().catch(console.error)
